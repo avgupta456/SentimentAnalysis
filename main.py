f41e1d1de34fdc833bfcdaad84b5e711fa7588d8
Abhijit Gupta <avgupta456@gmail.com>
2020-03-23 23:10:47 -0400

    upgraded loadTrainingDataSet()

--- main.py
+++ main.py
@@ -41,27 +41,20 @@ def buildTrainingSet(corpusFile, tweetDataFile):
         lineReader = csv.reader(csvfile, delimiter=',', quotechar="\"")
         for row in lineReader: corpus.append({"tweet_id":row[2], "label":row[1], "topic":row[0]})
 
-    rate_limit = 180
-    sleep_time = 900/180
-
-    trainingDataSet = []
+    sleep_time = 900/180 #rate limit
 
     for tweet in corpus:
         try:
+            csvfile = open(tweetDataFile, 'a', newline='')
+            linewriter = csv.writer(csvfile, delimiter=',', quotechar="\"")
+
             status = twitter_api.GetStatus(tweet["tweet_id"])
-            print("Tweet fetched" + status.text)
-            tweet["text"] = status.text
-            trainingDataSet.append(tweet)
+            print("Tweet fetched: " + status.text)
+
+            linewriter.writerow([tweet["tweet_id"], status.text, tweet["label"], tweet["topic"]])
+            csvfile.close()
+
             time.sleep(sleep_time)
         except: continue
-
-    # now we write them to the empty CSV file
-    with open(tweetDataFile, 'wb') as csvfile:
-        linewriter = csv.writer(csvfile,delimiter=',',quotechar="\"")
-        for tweet in trainingDataSet:
-            try: linewriter.writerow([tweet["tweet_id"], tweet["text"], tweet["label"], tweet["topic"]])
-            except Exception as e: print(e)
-
-    return trainingDataSet
 
 trainingData = buildTrainingSet(corpusFile, tweetDataFile)
